ffb62fd0feeaba6e746e5d7b33bede04866f8098
Pamela Shaw <Pamela.a.shaw@kp.org>
2024-08-03 08:58:49 -0700

    Add files via upload

---
+++ AllPackages.R
@@ -0,0 +1,10 @@
+library(MASS) ### stuff
+library(mvtnorm) ## for data generation
+library(survey) ### need to use sandwich2stage
+library(devtools) ### need to install sandwich2stage
+library(boot) # for bootstrap SE
+library(simex) ## Simex 
+library(simexaft) ## Hands on session
+library(openxlsx) ## to read excel files
+library(episensr) ## For misclassification
+
